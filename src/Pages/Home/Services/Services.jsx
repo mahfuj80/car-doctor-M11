9eaaefe4a213d1bfea0fd6b7dd7ef4572178d3ae
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-24 00:56:42 +0600

    Load Services fake data and display service Card

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data);
+      });
+  }, []);
+  return (
+    <div className="py-16">
+      <div className="text-center space-y-4">
+        <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
+        <h2 className="text-5xl">Our Service Area</h2>
+        <p className="lg:w-1/2 mx-auto">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which do not look even slightly
+          believable.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {services?.map((service) => (
+            <ServiceCard key={service?._id} service={service}></ServiceCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;

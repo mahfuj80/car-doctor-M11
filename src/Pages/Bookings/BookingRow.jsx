6e2ca3b4fce99dbf3dfea950a3e5a9946b67cd7c
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-27 02:32:50 +0600

    update booking using Patch

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
-const BookingRow = ({ booking, handleDelete }) => {
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
   // eslint-disable-next-line react/prop-types
-  const { _id, date, service, price, img } = booking;
+  const { _id, date, service, price, img, status } = booking;
 
   return (
     <tr>
@@ -35,7 +35,16 @@ const BookingRow = ({ booking, handleDelete }) => {
       <td>{date}</td>
       <td>{price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        {status === 'confirm' ? (
+          <span className="font-bold text-primary">Confirmed</span>
+        ) : (
+          <button
+            onClick={() => handleBookingConfirm(_id)}
+            className="btn btn-ghost btn-xs"
+          >
+            Please Confirm
+          </button>
+        )}
       </th>
     </tr>
   );

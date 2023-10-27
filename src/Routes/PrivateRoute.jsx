00cea7b0028d9ea230ea5672ea58fdc9fa8c4a84
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-27 17:49:49 +0600

    Auth Redirect added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,17 +1,18 @@
 import { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
   if (loading) {
     return <progress className="progress w-56"></progress>;
   }
   if (user?.email) {
     return children;
   }
-  return <Navigate to={'login'}></Navigate>;
+  return <Navigate to={'/login'} state={location?.pathname}></Navigate>;
 };
 
 export default PrivateRoute;
